9909b21ae30099b4329a8d1819d2694c85746649
cliu314 <clareliu@mit.edu>
2015-01-27 22:37:36 -0500

    added board

---
+++ square.h
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Square{
+	vector<string> moves;
+	int pos;
+public:
+	Square(int);
+	int get_pos();
+	vector<string> get_moves();
+	void add(string);
+	void clear();
+	char get_val(int);
+	string format_string(vector<string>);
+};
+
+Square::Square(int pos){
+	this->pos = pos;
+}
+
+int Square::get_pos(){
+	return this->pos;
+}
+
+void Square::add(string next){
+	this->moves.push_back(next);
+}
+
+void Square::clear(){
+	this->moves.clear();
+}
+
+vector<string> Square::get_moves(){
+	return this->moves;
+}
+
+char Square::get_val(int i){
+	if (this->moves.size() > i){
+		return this->moves[i][0];
+	}
+	return 0;
+}
+
+string Square::format_string(vector<string> moves){
+	int i;
+	string moves_list = "";
+
+	for (i = 0; i < moves.size(); i++){
+		moves_list += moves[i];
+
+		if ((i + 1)%3 == 0){
+			moves_list += "\n\n";
+		}
+		else {
+			moves_list += "  ";
+		}
+	}
+	return moves_list;
+}
