474bfd8a72ab7a01ef2f1308bd62d02add1b1d9e
cliu314 <clareliu@mit.edu>
2015-01-28 23:41:19 -0500

    got rid of some errors

--- quantumboard.h
+++ quantumboard.h
@@ -4,7 +4,7 @@
 #include <limits.h>
 #include <algorithm>
 #include <vector>
-#include <quantumgraph.h>
+#include "quantumgraph.h"
 
 using namespace std;
 
@@ -22,10 +22,13 @@ class tictactoe_game //controls the game
 {
 public:
     qboard_status qstatus; // initalizes the array that records the status of the quantum board
-    superqboard_status player_chooses_collapse(qgraph.nodelist list);//
+    superqboard_status player_chooses_collapse(list<int> list);//
+    int findstartingelem(int, int);
+    void update(int[3], tictactoe_player, QGraph);
+    void start(tictactoe_player);
 };
 
-tictactoe_game::tictactoe_game(void);
+//tictactoe_game::tictactoe_game(void);
 
 void tictactoe_game::start(tictactoe_player const player)
 {
@@ -36,7 +39,7 @@ void tictactoe_game::start(tictactoe_player const player)
             qstatus[j][k]=0; // assign 0 to each cell (means no one has made a move)
         }
     }
-    playing = true;
+    bool playing = true;
 }
 
 struct tictactoe_cell //indexes each vector representing a cell by its row and column
@@ -54,23 +57,22 @@ struct tictactoe_cell //indexes each vector representing a cell by its row and c
     }
 };
 
-void tictactoe_game::update(int[3] getplayermove, tictactoe_player const player, Qgraph qgraph)
+void tictactoe_game::update(int getplayermove[3], tictactoe_player player, QGraph qgraph)
 {
-    cell1=getplayermove[1];
-    cell2=getplayermove[2];
-    turnnumber=getplayermove[3]; //get info about what the player did
-     qstatus[cell1][turnnumber] = player;
+    int cell1=getplayermove[1];
+    int cell2=getplayermove[2];
+    int turnnumber=getplayermove[3]; //get info about what the player did
+    qstatus[cell1][turnnumber] = player;
     qstatus[cell2][turnnumber] = player;
 }
 
-superqboard_status tictactoe_game::player_chooses_collapse(qgraph.nodelist list) const
+superqboard_status tictactoe_game::player_chooses_collapse(list<int> list)
 {
     //This function generates two options for how to collapse the quantum board
     superqboard_status twooptions;
 
-    for (int k=1; k<list.size(), k++)
-    {
-        i=list(k);
+    for (int k=1; k<list.size(); k++){
+        int i=list(k);
         if (k<list.size()-1)
         {
             nextnode=list[i+1];
@@ -89,11 +91,12 @@ superqboard_status tictactoe_game::player_chooses_collapse(qgraph.nodelist list)
 
 int tictactoe_game::findstartingelem(int i, int nextnode)
 {
+    int startfromhere;
     for (int j=0; j<9; ++j)
     {
         if (qstatus[i][j]!=0 && qstatus[nextnode][j]==qstatus[i][j])
         {
-            int startfromhere=j;
+            startfromhere=j;
         }
     }
     return startfromhere;
